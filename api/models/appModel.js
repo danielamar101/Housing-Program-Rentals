c5c60a7caa1a12fca61388377209bee4b0653b0c
danielamar101 <danielamarble123456@gmail.com>
2021-08-13 01:55:09 -0400

    try again

--- api/models/appModel.js
+++ api/models/appModel.js
@@ -39,15 +39,15 @@ const inviteModel = connection.model('Invites',inviteSchema);
 const userModel = connection.model('Users', userSchema);
 const listingModel = connection.model('Listings',listingSchema);
 
-inviteModel.ensureIndexes();
-userModel.ensureIndexes();
-listingModel.ensureIndexes();
+// inviteModel.ensureIndexes();
+// userModel.ensureIndexes();
+// listingModel.ensureIndexes();
 
-const creatInvite = new inviteModel({ invite_code: 'natalie' });
-creatInvite.save(function (err) {
-  if (err) console.log(err);
+// const creatInvite = new inviteModel({ invite_code: 'natalie' });
+// creatInvite.save(function (err) {
+//   if (err) console.log(err);
   
-});
+// });
 
 
 module.exports = inviteModel;
