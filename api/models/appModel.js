e2fe3e41d8ff05b2ccf72a0c56304135b1c39429
danielamar101 <danielamarble123456@gmail.com>
2021-08-14 22:55:49 -0400

    removedd db iniation

--- api/models/appModel.js
+++ api/models/appModel.js
@@ -32,43 +32,43 @@ mongoose.model('User', UserSchema);
 mongoose.model('Listing',ListingSchema);
 
 
-const Invite = mongoose.model('Invite');
+// const Invite = mongoose.model('Invite');
 
-var oneThing = new Invite({invite_code: 'natalie'}).save().then(() => {
-    console.log("Saved Invite successfully")
-}).catch((error) => {
-    console.log("Error saving invite.." + error);
-});
+// var oneThing = new Invite({invite_code: 'natalie'}).save().then(() => {
+//     console.log("Saved Invite successfully")
+// }).catch((error) => {
+//     console.log("Error saving invite.." + error);
+// });
 
-const User = mongoose.model('User');
+// const User = mongoose.model('User');
 
-var twoThing = new User({
-    email: 'test',
-    password: 'test12'
-}).save().then(() => {
-    console.log("Saved User successfully")
-}).catch((error) => {
-    console.log("Error saving user.." + error);
-});
+// var twoThing = new User({
+//     email: 'test',
+//     password: 'test12'
+// }).save().then(() => {
+//     console.log("Saved User successfully")
+// }).catch((error) => {
+//     console.log("Error saving user.." + error);
+// });
 
-const Listing = mongoose.model('Listing');
+// const Listing = mongoose.model('Listing');
 
-var threeThing = new Listing({
-    address: '12 Gutheil Lane',
-    borough: 'Great Neck',
-    zip: '11024',
-    bedroom: '1',
-    bathroom: '1',
-    type: '1',
-    apt_num: '1',
-    ask_price: '69',
-    amenities: 'All',
-    //path to images of listing
-    images: ['/This/Is/A/Test']
-}).save().then(() => {
-    console.log("Saved listing successfully")
-}).catch((error) => {
-    console.log("Error saving listing.." + error);
-});
+// var threeThing = new Listing({
+//     address: '12 Gutheil Lane',
+//     borough: 'Great Neck',
+//     zip: '11024',
+//     bedroom: '1',
+//     bathroom: '1',
+//     type: '1',
+//     apt_num: '1',
+//     ask_price: '69',
+//     amenities: 'All',
+//     //path to images of listing
+//     images: ['/This/Is/A/Test']
+// }).save().then(() => {
+//     console.log("Saved listing successfully")
+// }).catch((error) => {
+//     console.log("Error saving listing.." + error);
+// });
 
 
